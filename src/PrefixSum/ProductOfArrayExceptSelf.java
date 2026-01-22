8f66716bb91d30f9ad7b3df3fd4fcf8c8907d63e
sartikmj <sartikmj@gmail.com>
2026-01-22 14:33:52 +0530

    Easier way for Product of Array Except Sell

--- src/PrefixSum/ProductOfArrayExceptSelf.java
+++ src/PrefixSum/ProductOfArrayExceptSelf.java
@@ -77,4 +77,30 @@ public class ProductOfArrayExceptSelf {
 
         return pre;
     }
+
+
+    //Easiest way by me
+    public int[] productExceptSelf3(int[] nums) {
+        int n = nums.length;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        int[] ans = new int[n];
+
+
+        pre[0] = 1;
+        for(int i=1;i<n;i++){
+            pre[i] = pre[i-1]*nums[i-1];
+        }
+
+        suf[n-1] = 1;
+        for(int i=n-2;i>=0;i--){
+            suf[i] = suf[i+1] * nums[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            ans[i] = pre[i] * suf[i];
+        }
+
+        return ans;
+    }
 }
