bcd71735f749b72a5a5680cedda018074ddcea32
sartikmj <sartikmj@gmail.com>
2026-01-22 15:11:27 +0530

    My approach to Search in Rotated Sorted Array

--- src/BinarySearch/Questions/SearchInRotatedSortedArray.java
+++ src/BinarySearch/Questions/SearchInRotatedSortedArray.java
@@ -49,6 +49,44 @@ public class SearchInRotatedSortedArray {
         return -1; //if there is no target in array.
     }
 
+    //Solution by Me (BEST FOR ME)
+    public int search1(int[] nums, int target) {
+        int pivot = 0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i+1]<nums[i]) pivot = i;
+        }
+
+        int p1=0,p2=pivot;
+        int p3=pivot+1, p4 = nums.length-1;
+        int min1=Integer.MAX_VALUE;
+        int low = p1, high = p2;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            else if(nums[mid]>target) high = mid-1;
+            else if(nums[mid]<target) low = mid+1;
+        }
+
+        low = p3;
+        high = p4;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            else if(nums[mid]>target) high = mid-1;
+            else if(nums[mid]<target) low = mid+1;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
 
     }
