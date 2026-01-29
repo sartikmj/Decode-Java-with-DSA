7b472340a5a1d6035115ddffa876ecd055537c51
sartikmj <sartikmj@gmail.com>
2026-01-29 18:06:29 +0530

    Three Sum Problem Leetcode 15

--- src/Array/StriverSheet/ThreeSum.java
+++ src/Array/StriverSheet/ThreeSum.java
@@ -1,46 +1,98 @@
 package Array.StriverSheet;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 //leetcode 15
 public class ThreeSum {
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
+    //Brute Force
+    // Using set to store the final ans so that we do not get duplicate triplets in the ans.
+    // TC=O(n^3) SC= 2*O(number of triplets)
+    public List<List<Integer>> threeSumBrute(int[] nums) {
         int n = nums.length;
-        for(int i=0;i<n-2;i++){
+//        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> list = new ArrayList<>(); //list of triplet
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        Collections.sort(list);
+                        //we need unique triplets not duplicates
+                        set.add(list); //HashSet auto removes duplicates
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
 
-            //skipping duplicated
+    //Better
+    // TC=O(n^2), SC=O(n^2), AS=O(n) -> HashSet
+    // The third element k = -(arr[i] + arr[j])
+    public List<List<Integer>> threeSumBetter(int[] nums) {
+        int n = nums.length;
+        Set<List<Integer>> ans = new HashSet<>();
+        for(int i=0;i<n;i++){
+            HashSet<Integer> set = new HashSet<>();
+            for(int j=i+1;j<n;j++){
+                List<Integer> list = new ArrayList<>();
+                int k = -(nums[i] + nums[j]);
+                if(set.contains(k)){
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(k);
+
+                    Collections.sort(list);
+                    ans.add(list);
+                }
+                set.add(nums[j]);
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+
+
+    //Optimized
+    // we will start with i=0, j=1, and k will always be at the end of the i.e. k=n-1
+
+    //TC=O(nlogn) + O(n^2) , SC=O(number of unique triplets)
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+
+            //skipping duplicate
             if(i>0 && nums[i] == nums[i-1]) continue;
 
-            int left = i + 1;
-            int right = nums.length - 1;
+            int j = i+1;
+            int k = n-1;
 
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0){
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    //skipping duplicates
-                    while(left<right && nums[left]==nums[left+1]) left++;
-                    while(left<right && nums[right] == nums[right-1]) right--;
-
-                    //skip the last duplication
-                    left++;
-                    right--;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0){
+                    j++;
                 }
-                else if(sum<0){
-                    left++;
+                else if(sum>0){
+                    k--;
                 }
-                else{ //sum>0
-                    right--;
+                else{ // sum==0
+                    List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(list);
+                    j++;
+                    k--;
+
+                    while( j<k && nums[j]==nums[j-1]) j++; //skipping duplicates
+                    while( j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
         return ans;
     }
+
 }
